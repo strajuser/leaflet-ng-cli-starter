4e88d45c27bb5941f76fa22f26fc2615b262a40b
Стражников Дмитрий Сергеевич <strazhnikovds@sibintek.ru>
2017-07-04 13:16:45 +0300

    - proj4 import examle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as proj4x from 'proj4';
+let proj4 = (proj4x as any).default;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +12,8 @@ export class AppComponent implements OnInit {
   title = 'app';
 
   ngOnInit() {
+    console.log(proj4);
+
     const map = L.map('map').setView([51.505, -0.09], 13);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
